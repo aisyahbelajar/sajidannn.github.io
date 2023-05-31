b941f4fd09f9feb6518b4b209fc63c565bb6f2fb
sajidannn <ahmadnursajidan@gmail.com>
2023-05-31 08:40:54 +0700

    tambah keterangan salah benarnya jawaban

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const articles = [
   {
     headline: "Kemunculan Babi Ngepet",
     source:
-      "Pada video yang sempat viral di bawah, menunjukkan bahwa adanya babi ngepet yang berkeliaran dipemukiman warga, dan warga juga kerap kehilangan barang berharga mereka. setelah babi tersebut tertangkap warga meyakini bahwa babi tersebut adalah siluman. Menurut Kamu apakah berita dalam postingan ini palsu atau asli?",
+      "Pada video yang sempat viral di bawah, menunjukkan bahwa adanya babi ngepet yang berkeliaran dipemukiman warga, dan warga juga kerap kehilangan barang berharga mereka. setelah babi tersebut tertangkap warga meyakini bahwa babi tersebut adalah siluman. Menurut Kamu apakah berita dalam postingan ini hoaks atau fakta?",
     video: "content/babi ngepet.mp4",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "video di atas ternyata hoax (tidak benar). Karena babi yang di tangkap warga tersebut dibeli secara online dengan harga Rp 900.000, kemudian tokoh kampung daerah itu membuat sekenario seolah-olah itu adalah makhluk jadi-jadian.",
     answer: false,
@@ -12,9 +12,9 @@ const articles = [
   {
     headline: "Anak Dikutuk Jadi Ikan Pari",
     source:
-      "Pada vidio dibawah, terdapat suatu makhluk yang anggap dikutuk karena makhluk itu durhaka pada orang tuanya dan juga memiliki wajah yang mirip manusia. Menurut hal tersebut palsu atau nyata?",
+      "Pada vidio dibawah, terdapat suatu makhluk yang anggap dikutuk karena makhluk itu durhaka pada orang tuanya dan juga memiliki wajah yang mirip manusia. Menurut hal tersebut hoaks atau nyata?",
     video: "content/ikan pari.mp4",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "Wujudnya mungkin memang mirip seperti manusia, namun faktanya hewan itu bukanlah makhluk yang dikutuk seperti yang dikisahkan. Hewan viral itu adalah ikan pari jenis shovelnose Guitarfish",
     answer: false,
@@ -22,9 +22,9 @@ const articles = [
   {
     headline: "Permen Narkoba",
     source:
-      "Pada postingan tersebut, menjelaskan bahwa permen berbetuk seperti dot adalah bentuk baru dari narkoba dan sudah beredar di sekolah-sekolah SD. Menurut Kamu apakah postingan ini palsu atau betul?",
+      "Pada postingan tersebut, menjelaskan bahwa permen berbetuk seperti dot adalah bentuk baru dari narkoba dan sudah beredar di sekolah-sekolah SD. Menurut Kamu apakah postingan ini hoaks atau betul?",
     image: "content/permendot.jpg",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "Terkait hal tersebut, Kepala Badan Narkotika Nasional (BNN) Budi Waseso menyatakan, permen Dot negatif dari bahan yang mengandung narkotika. Permen Dot kami nyatakan negatif, clear dari bahan kimia narkotika. Ini sudah dibuktikan tiga instansi, BNN, Polisi dan Laboratorium POM, kata Budi Waseso di Kantor BNN, Jakarta Timur, Kamis (16/3/2017).",
     answer: false,
@@ -34,7 +34,7 @@ const articles = [
     source:
       "Pada gambar di bawah, akun Tik Tok @NETIZEN +62 membagikan postingan yang menyebut bahwa vaksin COVID-19 mengandung microchip untuk kepentingan pemerintah luar. Apakah hal tersebut merupakan berita fakta atau hoax?",
     image: "content/microchip.jpg",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "Berdasarkan penelusuran, klaim penanaman microchip ke tubuh seluruh penduduk dunia lewat vaksin Covid-19 adalah tidak benar. Disamping itu, tidak ada bukti yang menunjukkan bahwa vaksin ini akan memiliki chip Radio-Frequency-Identification (RFID).",
     answer: false,
@@ -44,9 +44,9 @@ const articles = [
     source:
       "pada video yang sempat viral di bawah, menunjukkan bahwa adanya selaput berwarna putih di cangkang telur tersebut yang dianggap plastik. Menurut Kamu apakah hal yang disampaikan dalam video tersebut memang benar?",
     video: "content/telur plastik.mp4",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
-      "Bareskrim Polri menegaskan, video mengenai telur palsu yang baru-baru saja beredar tidaklah benar. Dia mengatakan, informasi mengenai telur palsu beredar di Indonesia adalah hoaks.",
+      "Bareskrim Polri menegaskan, video mengenai telur hoaks yang baru-baru saja beredar tidaklah benar. Dia mengatakan, informasi mengenai telur hoaks beredar di Indonesia adalah hoaks.",
     answer: false,
   },
   {
@@ -54,7 +54,7 @@ const articles = [
     source:
       "Foto di bawah menunjukkan pesan melalui WA (Whats App) yang meyebutkan BPJS memberi bantuan untuk biaya kesehatan sampai pembangunan rumah. Menurut kamu apakah informasi tersebut dapat dipercaya dan benar demikian?",
     image: "content/bantuanbpjs.jpg",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "Faktanya, BPJS Kesehatan melalui akun resminya menyatakan bahwa hal tersebut tidak benar atau hoaks. Sebab BPJS Kesehatan tidak pernah memberikan bantuan dana tunai kepada peserta JKN-KIS. Selain itu, BPJS Kesehatan juga mengimbau masyarakat agar selalu waspada terhadap segala informasi hoaks dan penipuan yang mengatasnamakan BPJS Kesehatan.",
     answer: false,
@@ -64,7 +64,7 @@ const articles = [
     source:
       "Pada vidio dibawah, terdapat dua orang pemuda yang membuang isi dari tabung gas 3 kg dan mereka berkata bahwa, Ini gas isinya air, bukan gas. ungkap pria dalam video. Menurut Kamu apakah pernyataan yang disampaikan dalam video tersebut dapat dipercaya dan kenyataan?",
     video: "content/LPG.mp4",
-    cek: "Berita ini Palsu",
+    cek: "informasi ini hoaks",
     explain:
       "Arti dari singkatan LPG adalah Liquefied Petroleum Gas, yang berarti gas yang dicairkan. Jadi isi dari LPG memang berbentuk seperti air. Dan tindakan dalam video merupakan hal yang berbahaya",
     answer: false,
@@ -72,9 +72,9 @@ const articles = [
   {
     headline: "Timnas MLBB Women Juarai SEA Games 2023",
     source:
-      "PBESI di akun instagramnya memposting berita yang berisi bahwa tim MLBB Women Indonesia berhasli meraih medali emas di SEA Games 2023. Menurut Anda apakah postingan ini dapat dipercaya atau tidak?",
+      "PBESI di akun instagramnya memposting berita yang berisi bahwa tim MLBB Women Indonesia berhfakta meraih medali emas di SEA Games 2023. Menurut Anda apakah postingan ini dapat dipercaya atau tidak?",
     image: "content/mlbb women.jpg",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Di SEA Games 2023, timnas MLBB Women Indonesia memeang berhasil membawa pulang medali emas usai mengalahkan timnas Filipina dalam laga Best of 5 (BO5) dengan skor tipis 3-2.",
     answer: true,
@@ -84,7 +84,7 @@ const articles = [
     source:
       "Postingan di bawah berisi berita yang mennyatakan bahwa Menkominfo Jhonny G PLate, menjadi tersangka atas tindak pidana kasus korupsi. Menurut anda apakah yang disampaikan dalam berita tersebut merupakan informasi fakta?",
     video: "content/jhonny yes papa.mp4",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Kejaksaan Agung resmi menetapkan Menteri Komunikasi dan Informatika Johnny Plate sebagai tersangka dugaan korupsi. Dia juga langsung ditahan selama 20 hari di Rutan Salemba Cabang Kejagung.",
     answer: true,
@@ -94,7 +94,7 @@ const articles = [
     source:
       "Video di bawah berita yang berisi himbauan Presiden Jokowi tentang resiko terjadinya kenaikan harga pangan akibat cuaca ekstream El Nino. Menurut anda apakah hal yang disampaikan memang nyata?",
     video: "content/elnino.mp4",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Hal yang disampaikan memang nyata. Badai Elnino memang dapat menyebabkan resiko krisis pangan di Indonesia.",
     answer: true,
@@ -104,7 +104,7 @@ const articles = [
     source:
       "Pada portal berita lensaIndonesia.com memposting berita yang berisi bahwa seorang prngrajin sepatu dapat meraup untung Rp 15 juta per bulan hanya karena iseng. Menurut Anda apakah hal tersebut fakta?",
     image: "content/pengrajin sepatu.jpg",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Usaha Ida Yudha yang diteruskan oleh generasi kedua (Victoria) ini memang sudah menembus pasar dunia. Ida Yudha Kusuma Dewi sukses hasilkan ribuan sepatu bordir dan rajut dengan meraup omset rata-rata Rp. 15 juta / bulan.",
     answer: true,
@@ -114,7 +114,7 @@ const articles = [
     source:
       "Pada portal berita news.republika.com memposting berita yang berisi pemerintahan Lampung gencar memperbaiki beberapa titik jalan rusak dikarenakan viral dan juga kunjungan Presiden jokowi.",
     image: "content/lampung.jpg",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Perbaikan jalan rusak di Lampung gencar dilakukan karena beredar info pada pekan tersebut, Presiden Joko Widodo (Jokowi) akan meninjau jalan rusak di Lampung, sehingga pemerintah daerah pun bergerak cepat melakukan perbaikan.",
     answer: true,
@@ -124,7 +124,7 @@ const articles = [
     source:
       "Video di bawah berisi informasi bahwa Timnas Indonesia akan bertanding melawan Timnas Argentina di Jakarta. Apakah hal tersebut dapat dipercaya dan merupakan informasi fakta? ",
     video: "content/argentina.mp4",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Timnas Indonesia memang akan bertanding dengan Timnas Indonesia di Jakarta pada 19 Juni 2023 dan telah dikonfirmasi oleh Menteri Olahraga Erick Thohir.",
     answer: true,
@@ -134,7 +134,7 @@ const articles = [
     source:
       "Video di bawah menunjukkan seorang artis yakni Aldi Taher mencalonkan dirinya sebagai calon legislatif dengan diusung oleh 2 partai ternama. Menurut anda apakah informasi tersebut memang benar?",
     video: "content/aldi taher.mp4",
-    cek: "Berita ini Asli",
+    cek: "informasi ini fakta",
     explain:
       "Informasi tersebut memang benar. Aldi Taher sendiri telah mengonfirmasi hal tersebut dalam siaran langsung dirinya dengan salah satu acara saluran televisi TV One",
     answer: true,
@@ -216,7 +216,7 @@ function cekScore() {
   if (score >= (totall * 80) / 100) {
     petasan();
   } else {
-    document.getElementById("ucapan").textContent = "TETAP SEMANGAT!!";
+    snow();
   }
 }
 
@@ -297,6 +297,11 @@ function checkAnswer(userAnswer) {
 
     if (isCorrect) {
       score++;
+      document.getElementById("cek").textContent =
+        "✅Jawaban kamu benar, " + currentArticle.cek;
+    } else {
+      document.getElementById("cek").textContent =
+        "❌Jawaban kamu salah, " + currentArticle.cek;
     }
 
     updateScore();
